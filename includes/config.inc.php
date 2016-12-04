921152a8b927791b4a5c9d3be20b40bf95f74e26
Farzi <fa.zarei@gmail.com>
2016-12-04 22:23:07 +1100

    Server_Change

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -4,7 +4,8 @@ $DB_Name="ekp_hosting2";
 $DB_User="ekp-hosting";
 $DB_Pwd="ekp-hosting";
 
-$logsfolder="/usr/local/www/vline.globalvision.com.au/public_html/vlineappadmin/logs/";
+//$logsfolder="/usr/local/www/vline.globalvision.com.au/public_html/vlineappadmin/logs/";
+$logsfolder="/var/www/vline.globalvision.com.au/public_html/vlineappadmin/logs/";
 $shared_key="shanfarz115ht1";
 
 ?>
